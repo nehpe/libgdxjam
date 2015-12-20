94dbcd671f5cd88c077110ca9947f9839d933e03
nehpe <n@nehpe.com>
2015-12-19 20:06:05 -0700

    Base Game setup

---
+++ desktop/src/com/nehpe/spaceminer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nehpe.spaceminer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nehpe.spaceminer.SpaceMinerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceMinerGame(), config);
+	}
+}
